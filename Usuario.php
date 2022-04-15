b7914707bc7b6f986956235c7120425bc4a61db4
heliobrandao <heliob79@gmail.com>
2022-04-15 20:04:36 -0300

    lesson_01

---
+++ Usuario.php
@@ -0,0 +1,38 @@
+<?php
+namespace Alura;
+
+class Usuario
+{
+    private $nome;
+    private $sobrenome;
+
+    public function __construct(string $nome) {
+
+      $nomeSobrenome = explode(" ", $nome, 2);
+
+      if ($nomeSobrenome[0] === ""){
+        $this->nome = "Nome inválido";
+      } else {
+        $this->nome = $nome;
+      }
+
+      if ($nomeSobrenome[1] === null){
+        $this->sobrenome = "Sobrenome inválido";
+      } else {
+        $this->sobrenome = $sobrenome;
+      }
+
+
+    }
+
+    public function getNome(): string 
+    {
+      return $this->nome;
+    }
+
+    public function getSobrenome(): string 
+    {
+      return $this->sobrenome;
+    }
+
+}
